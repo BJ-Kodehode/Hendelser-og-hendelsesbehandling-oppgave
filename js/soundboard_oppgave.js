c4a39efedb47a6149518b970cd3c13bb66eedd68
BJ-Kodehode <bernt25sj@gmail.com>
2025-03-04 13:19:54 +0100

    oppdatert soundeboard oppgave

--- js/soundboard_oppgave.js
+++ js/soundboard_oppgave.js
@@ -1,82 +1,52 @@
-//! Comment like this can be removed after finishing the tasks
+import { sounds } from "./sounds.js";
 
-//*1. Create an external JSON or js file containing information about the sounds you want to use. Import the file in here: */
+// 1.1 Catch the HTML element with id drumkit
+const drumkit = document.getElementById("drumkit");
 
-//* If using json file: */
-// fetch it through a function using a syntax like this:
-/* async function fetchData() {
-    const response = await fetch("path/to/file.json");
-    const data = await response.json();
-    return data;
-}  */
+// 1.2 Console log the drumkit element
+console.log(drumkit);
 
-//* If using js file: */
-// import the file using a syntax like this:
-/* import { sounds } from "./path/to/file.js"; */
+// 2. Console log the fetched sounds
+console.log(sounds);
 
-//*1.1. Catch the html element with id drumkit: */
+// 2.2 Console log the keys of the sounds
+sounds.forEach(sound => console.log(sound.key));
 
-//! write code for part 1 here
-
-//*1.2 console.log the drumkit element: */
-
-//! write code for part 1.2 here
-
-//*2 Write a console log for the fetched sounds so you know how the structure is and how you can use it */
-
-//! write code for part 2 here
-
-//*2.2 Write a console log for the fetched sounds keys so you know to use the different parts of the data */
-
-//! write code for part 2.2 here
-
-//*3. Finish the function below to create a functioning soundboard:: */
-
+// 3. Create a functioning soundboard
 function drumComponent(sound) {
-  //* 3.1. finish the buttenEl variable that should create a button element with .createElement
+    // 3.1 Create button element
+    const buttonEl = document.createElement("button");
+    buttonEl.textContent = sound.name;
+    buttonEl.classList.add("sound-button");
 
-  const buttonEl = document;
+    // 3.2 Create audio element
+    const audioEl = document.createElement("audio");
+    audioEl.src = sound.file;
+    audioEl.id = sound.key;
 
-  // add textContent to the created buttonElement. Textcontent should be either the file name and/or key needed to be pressed
+    // 3.3 Add event listener for keydown
+    document.addEventListener("keydown", (event) => {
+        if (event.key.toLowerCase() === sound.key) {
+            audioEl.currentTime = 0;
+            audioEl.play();
+        }
+    });
 
-  //3.2. make a variables that create an audio element with .createElement
-  //the audio element that is created should have the src equal to the file source
-  //the audio element that is created should have the id equal to the textcontent created in 3.1.
+    // 3.5 Add event listener for button click
+    buttonEl.addEventListener("click", () => {
+        audioEl.currentTime = 0;
+        audioEl.play();
+    });
 
-  //! add the audioEl.id here
-
-  //*3.3. OPTIONAL. finish the eventlistner to the whole page (document) that should:
-  //active when pressing a keyboard key (first parameter of the eventlistener)
-
-  document.addEventListener("???", (event) => {
-    // finish the key variable that refers to the key pressed
-    const key = event;
-    // make a conditional logic that asks if the variable created just above is the same as the sound key that should be pressed (the key "key" in the js or JSON data you created)
-    if (key === "the key referanse in the data you are using") {
-      // code for playing the audio element
-    }
-  });
-
-  //* 3.4. OPTIONAL. If you used keydown as the first parameter in the previous eventlistener, add another eventlistner to the whole page that:
-  //actives when releasing a keyboard key (first parameter of the eventlistener)
-  document.addEventListener("???", (event) => {
-    // code that pauses the audio element
-    // code that sets the current time of the audio element to 0
-  });
-
-  //3.5. Create an eventlistener for clicking. OPTIONAL: Also create a logic for preventing more sounds to be played at the same time
-  buttonEl.addEventListener("???", () => {
-    // code that plays the audioEl
-  });
-  //3.6. append the created button and audio element to the html element you refered in 1.
-  // code that appends the buttonEl and audioEl to the drumkit element
+    // 3.6 Append button and audio to drumkit
+    drumkit.appendChild(buttonEl);
+    drumkit.appendChild(audioEl);
 }
 
-//*4. Create a function that loops over the sounds (from the data file you created). Use that function created in 3. to use the logic there to create the buttons. I prefer that you use .forEach or .map */
+// 4. Loop over sounds and create buttons
 function createDrumKit() {
-  // forEach logic that loops over each sound in the data you are using
-  // each time the loops run it should call the drumComponent function
+    sounds.forEach(drumComponent);
 }
 
-//*4.1 Call on the function that loops over the sounds and creates the buttons */
-// call the function from part 4 here
+// 4.1 Call function to generate buttons
+createDrumKit();
