c42ff95050846f60de6e446b061b823bb9823fa1
BJ-Kodehode <bernt25sj@gmail.com>
2025-03-05 11:09:16 +0100

    oppdatert soundboard

--- data/soundsJSexample.js
+++ data/soundsJSexample.js
@@ -2,5 +2,11 @@ export const sounds = [
   { name: "Fart", file: "./fart.mp3", key: "f" },
   { name: "Shut Up", file: "./shutup.mp3", key: "p" },
   { name: "WOW", file: "./WOW MEME.mp3", key: "g" },
-  { name: "Yes King", file: "./yes-king.mp3", key: "y" }
+  { name: "Yes King", file: "./yes-king.mp3", key: "y" },
+  { name: "Airhorn", file: "./dj-airhorn.mp3", key: "h" },
+  { name: "Not Good", file: "./Not good.mp3", key: "j" },
+  { name: "Laugh", file: "./oh no laugh.mp3", key: "k" },
+  { name: "Panjabi", file: "./panjabi.mp3", key: "l" },
+  { name: "Pedro", file: "./pedro-song.mp3", key: "m" },
+  { name: "Run", file: "./RUN.mp33", key: "n" },
 ];
